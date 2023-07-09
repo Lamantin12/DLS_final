e5357b23790a093df3637081f0cb996bdc4078f5
lamantin12 <Khodosevich.Leo@gmail.com>
2023-07-09 11:51:47 +0300

    fix

--- model/usage.py
+++ model/usage.py
@@ -4,6 +4,7 @@ from torchvision import transforms
 import torch
 import model.models as models
 import io
+import gc
 torch.manual_seed(42)
 
 transform = transforms.Compose([
@@ -12,15 +13,15 @@ transform = transforms.Compose([
     ])
 
 def load_models(filepath):
-    G_model_A_B = models.CycleGANGenerator()
+    #G_model_A_B = models.CycleGANGenerator()
     F_model_B_A = models.CycleGANGenerator()
 
-    G_model_A_B.load_state_dict(torch.load(filepath+"G_AB_199.pth", map_location=torch.device('cpu')))
+    #G_model_A_B.load_state_dict(torch.load(filepath+"G_AB_199.pth", map_location=torch.device('cpu')))
     F_model_B_A.load_state_dict(torch.load(filepath+"G_BA_199.pth", map_location=torch.device('cpu')))
 
-    G_model_A_B.eval()
+    #G_model_A_B.eval()
     F_model_B_A.eval()
-    return G_model_A_B, F_model_B_A
+    return ..., F_model_B_A
 
 '''
 def display_in_out_rec(A_B_model, B_A_model, image, mode):
@@ -68,15 +69,16 @@ def process_image_from_filepath(A_B_model, B_A_model, filepath, mode):
 def im_proc(A_B_model, B_A_model, image, mode):
     if mode == 'A2B':
         out = A_B_model(image).detach().cpu()
-        rec = B_A_model(out).detach().cpu()
+        #rec = B_A_model(out).detach().cpu()
     else:
         out = B_A_model(image).detach().cpu()
-        rec = A_B_model(out).detach().cpu()
-    return image, out, rec
+        #rec = A_B_model(out).detach().cpu()
+    return out
 
 def process_image_from_image(A_B_model, B_A_model, image, mode):
     image = PIL.Image.open(io.BytesIO(image.read()))
-    im, out, _ = im_proc(A_B_model, B_A_model, transform(image), mode)
+    out= im_proc(A_B_model, B_A_model, transform(image), mode)
+    gc.collect()
     return transforms.ToPILImage()(out*0.5 + 0.5)
     fig, ax = plt.subplots(1, 1)
     ax.imshow(transforms.ToPILImage()(im*0.5 + 0.5))
